2a1c498fb94170f1a87c0384522f90135832a666
syariffff <syarieff118@gmail.com>
2024-04-22 22:55:40 +0700

    Ini buat presentasi pertama ya ges

---
+++ src/projectgeometri/Geometri2D/Persegi.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectgeometri.Geometri2D;
+import projectgeometri.Benda2D;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Persegi implements Benda2D{
+
+    @Override
+    public double hitungLuas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double hitungKeliling() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double hitungPanjang() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double hitungLebar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void tampilkanInfo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
